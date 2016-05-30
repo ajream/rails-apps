cedc4659d58813d25ec3e1f5a2b645d174b7312d
ajream <ajream.gd@gmail.com>
2016-05-30 11:57:09 +0800

    added nickname to users

--- demo_01/db/schema.rb
+++ demo_01/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530021404) do
+ActiveRecord::Schema.define(version: 20160530033530) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160530021404) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
