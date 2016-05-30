524ade7ed6fa2992390838c8ac910a4ec3b7bda5
ajream <ajream.gd@gmail.com>
2016-05-30 11:33:12 +0800

    added comments

--- demo_01/config/routes.rb
+++ demo_01/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   resources :links do
   	member do 
   		put 'like', 		to: "links#up_vote"
   		put 'dislike', 	to: "links#down_vote"
   	end
+    resources :comments
   end
   root to: 'links#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
