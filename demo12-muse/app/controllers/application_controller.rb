972a12bbee17472113b5e5965147fdc7ff74ed81
ajream <ajream.gd@gmail.com>
2016-09-29 23:54:01 +0800

    add name to users.

--- demo12-muse/app/controllers/application_controller.rb
+++ demo12-muse/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  	# 添加了name字段，需要做一下设置才可以修改name字段。
+  	def configure_permitted_parameters
+	    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+	    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+	  end
 end
