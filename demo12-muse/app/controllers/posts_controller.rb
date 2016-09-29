29eaba6b222a2be63cb8041ddcc9f26c5583f2e0
ajream <ajream.gd@gmail.com>
2016-09-30 00:20:14 +0800

    setup paperclip for post image upload.

--- demo12-muse/app/controllers/posts_controller.rb
+++ demo12-muse/app/controllers/posts_controller.rb
@@ -41,9 +41,6 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    puts "*"*88
-    p @post
-    p post_params
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
@@ -73,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :link, :description)
+      params.require(:post).permit(:title, :link, :description, :image)
     end
 end
